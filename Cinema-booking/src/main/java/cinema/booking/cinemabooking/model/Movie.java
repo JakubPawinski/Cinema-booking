97331bf73fb0ab1948111f1b0a77fb835cdd00de
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-12-22 19:51:23 +0100

    feat(db): add database configuration and connection

--- Cinema-booking/src/main/java/cinema/booking/cinemabooking/model/Movie.java
+++ Cinema-booking/src/main/java/cinema/booking/cinemabooking/model/Movie.java
@@ -2,8 +2,6 @@ package cinema.booking.cinemabooking.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import java.util.Set;
 
--- Cinema-booking/src/main/java/cinema/booking/cinemabooking/model/User.java
+++ Cinema-booking/src/main/java/cinema/booking/cinemabooking/model/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
